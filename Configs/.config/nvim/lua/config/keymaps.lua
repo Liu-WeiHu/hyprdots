590dd84bfca7aa92f53c296eb9f43e3fb32aecd9
Liu-WeiHu <6460176@qq.com>
2024-07-08 10:42:29 +0800

    update nvim config keymap

--- Configs/.config/nvim/lua/config/keymaps.lua
+++ Configs/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 -- vim.keymap.del({ "n", "x", "o" }, "s")
 -- vim.keymap.del({ "n", "x", "o" }, "S")
+-- 使用黑洞寄存器配置 s, S, c, C 命令
+vim.api.nvim_set_keymap('n', 's', '"_s', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'S', '"_S', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'c', '"_c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'C', '"_C', { noremap = true, silent = true })
+
+-- 处理可视模式下的情况
+vim.api.nvim_set_keymap('v', 's', '"_s', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'S', '"_S', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'c', '"_c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'C', '"_C', { noremap = true, silent = true })
