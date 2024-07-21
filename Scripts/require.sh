fa51bc9b074be7be062b4de81eba60fa7e607f4c
Liu-WeiHu <6460176@qq.com>
2024-07-21 10:54:04 +0800

    update

--- Scripts/require.sh
+++ Scripts/require.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sed -i 	'/export g_margin=/cexport g_margin=\$(( b_height*20/100 )) 
-         /export s_fontpx=/cexport s_fontpx=\$(( b_height*45/100 )) 
+sed -i '/export g_margin=/cexport g_margin=\$(( b_height*20/100 ))
+         /export s_fontpx=/cexport s_fontpx=\$(( b_height*45/100 ))
          /export x3g_margin=/c\        export x3g_margin=0' /home/liu/Documents/hyprdots/Configs/.local/share/bin/wbarstylegen.sh
 
-cat << EOF >> /home/liu/Documents/hyprdots/Configs/.config/waybar/config.ctl
+cat <<EOF >>/home/liu/Documents/hyprdots/Configs/.config/waybar/config.ctl
 0|28|top|( clock tray ) ( hyprland/workspaces wlr/taskbar )|( hyprland/window )|( network cpu memory ) ( backlight pulseaudio battery pulseaudio#microphone custom/cliphist custom/notifications custom/power )
 EOF
 
@@ -14,20 +14,24 @@ sed -i '/"max-length"/c\        "max-length": 60' /home/liu/Documents/hyprdots/C
 sed -i '/"format-wifi"/c\        "format-wifi": "󰇚{bandwidthDownBytes} 󰕒{bandwidthUpBytes}",' /home/liu/Documents/hyprdots/Configs/.config/waybar/modules/network.jsonc
 sed -i '/"format-ethernet"/c\        "format-ethernet": "󰇚{bandwidthDownBytes} 󰕒{bandwidthUpBytes}",' /home/liu/Documents/hyprdots/Configs/.config/waybar/modules/network.jsonc
 
-cat << EOF >> /home/liu/Documents/hyprdots/Configs/.config/gtk-3.0/settings.ini
+cat <<EOF >>/home/liu/Documents/hyprdots/Configs/.config/gtk-3.0/settings.ini
 gtk-im-module=fcitx
 EOF
 
 sed -i '/font_size/cfont_size 10.0' /home/liu/Documents/hyprdots/Configs/.config/kitty/kitty.conf
 
-sed -i '/bind = \$mainMod, mouse_down/cbind = \$mainMod, mouse_down, workspace, e-1 
-        /bind = \$mainMod, mouse_up/cbind = \$mainMod, mouse_up, workspace, e+1' /home/liu/Documents/hyprdots/Configs/.config/hypr/keybindings.conf
+sed -i '/bind = \$mainMod, mouse_down/cbind = \$mainMod, mouse_down, workspace, e-1
+        /bind = \$mainMod, mouse_up/cbind = \$mainMod, mouse_up, workspace, e+1
+        /fullscreen/cbind = $mainMod, Return, fullscreen
+        /screenshot.sh/s/^/# /
+        /Screencapture/a\bind = Alt, Q, exec, $scrPath/screenshot.sh sf\
+        bind = Alt, W, exec, $scrPath/screenshot.sh m' /home/liu/Documents/hyprdots/Configs/.config/hypr/keybindings.conf
 
-sed -i '/misc/a\    mouse_move_enables_dpms = true 
-        /misc/a\    key_press_enables_dpms = true 
-        /natural_scroll/c\        natural_scroll = true 
-        /touchpad/a\        scroll_factor = 0.3' /home/liu/Documents/hyprdots/Configs/.config/hypr/hyprland.conf  
+sed -i '/misc/a\    mouse_move_enables_dpms = true
+        /misc/a\    key_press_enables_dpms = true
+        /natural_scroll/c\        natural_scroll = true
+        /touchpad/a\        scroll_factor = 0.3' /home/liu/Documents/hyprdots/Configs/.config/hypr/hyprland.conf
 
-sed -i '/F10/d 
-        /F11/d 
+sed -i '/F10/d
+        /F11/d
         /F12/d' /home/liu/Documents/hyprdots/Configs/.config/hypr/keybindings.conf
