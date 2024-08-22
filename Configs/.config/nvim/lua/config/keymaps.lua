c9e62f0dfe2251543d4293e93b5d68645df1c0d3
Liu-WeiHu <6460176@qq.com>
2024-08-22 10:06:32 +0800

    update

--- Configs/.config/nvim/lua/config/keymaps.lua
+++ Configs/.config/nvim/lua/config/keymaps.lua
@@ -11,14 +11,14 @@ vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 -- 使用黑洞寄存器配置 s, S, c, C 命令
--- vim.keymap.set("n", "s", '"_s', { noremap = true, silent = true })
--- vim.keymap.set("n", "S", '"_S', { noremap = true, silent = true })
+vim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set("n", "D", '"_D', { noremap = true, silent = true })
 vim.keymap.set("n", "c", '"_c', { noremap = true, silent = true })
 vim.keymap.set("n", "C", '"_C', { noremap = true, silent = true })
 
 -- 处理可视模式下的情况
--- vim.keymap.set("v", "s", '"_s', { noremap = true, silent = true })
--- vim.keymap.set("v", "S", '"_S', { noremap = true, silent = true })
+vim.keymap.set("v", "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set("v", "D", '"_D', { noremap = true, silent = true })
 vim.keymap.set("v", "c", '"_c', { noremap = true, silent = true })
 vim.keymap.set("v", "C", '"_C', { noremap = true, silent = true })
 
@@ -27,3 +27,22 @@ vim.keymap.set("i", "<A-h>", "<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "<A-j>", "<Down>", { noremap = true, silent = true })
 vim.keymap.set("i", "<A-k>", "<Up>", { noremap = true, silent = true })
 vim.keymap.set("i", "<A-l>", "<Right>", { noremap = true, silent = true })
+
+-- 修改默认终端大小
+local lazyterm = function()
+    LazyVim.terminal(nil, {
+        cwd = LazyVim.root(),
+        size = { width = 0.7, height = 0.7 },
+        border = "rounded",
+        title = "码农万岁",
+        title_pos = "center",
+        style = "minimal",
+        persistent = true,
+        noautocmd = true,
+    })
+end
+vim.keymap.set("n", "<leader>ft", lazyterm, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("n", "<leader>fT", function()
+    LazyVim.terminal()
+end, { desc = "Terminal (cwd)" })
+vim.keymap.set("n", "<c-/>", lazyterm, { desc = "Terminal (Root Dir)" })
